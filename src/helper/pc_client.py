cf6bc0716f02a8e10b5ca829ba3d6c343cc81577
zloverty <liuzy19911112@gmail.com>
2025-10-06 17:08:26 +0800

    use local server to test

--- src/helper/pc_client.py
+++ src/helper/pc_client.py
@@ -2,8 +2,8 @@ import socket
 
 # --- 配置 ---
 # 关键点：这里填写你树莓派的真实IP地址！
-RASPBERRY_PI_IP = '192.168.1.106'  # <--- !!! 修改这里 !!!
-PORT = 12345                      # 必须与服务器端的端口一致
+RASPBERRY_PI_IP = '127.0.0.1'  # <--- !!! 修改这里 !!!
+PORT = 10001                      # 必须与服务器端的端口一致
 
 # --- 程序 ---
 print("--- 电脑客户端已启动 ---")
@@ -11,10 +11,9 @@ print(f"准备从 {RASPBERRY_PI_IP}:{PORT} 读取数据")
 
 # 创建 UDP socket
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-    
     # 1. 先向服务器发送一个启动信号，让服务器知道我们的地址
     s.sendto(b'start', (RASPBERRY_PI_IP, PORT))
-
+    
     # 2. 现在可以循环接收来自服务器的数据了
     print("正在接收数据...")
     while True:
