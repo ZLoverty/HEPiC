e8c20b6071102b4731aef075db966e57048a127f
zloverty <liuzy19911112@gmail.com>
2025-10-21 09:31:54 +0800

    reorganize the files, separate the server program to a new repo

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Config:
     version = "0.0.0"
     test_mode = True
     test_image_folder = "~/Documents/GitHub/etp_ctl/test/filament_images_simulated"
-    default_host = "192.168.0.107"
+    default_host = "192.168.114.48"
     data_frequency = 10 # Hz, defines how many data points per time
     tmp_data_maxlen = 100 # length of temporarily cached data
     final_data_maxlen = 10000000
