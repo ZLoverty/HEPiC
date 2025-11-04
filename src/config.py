5086051e8608836132849c5e0cdcd0dd910ccde2
zloverty <liuzy19911112@gmail.com>
2025-11-04 16:36:44 +0800

    use wired IP by default

--- src/config.py
+++ src/config.py
@@ -6,8 +6,8 @@ class Config:
     version = "0.0.0"
     test_mode = False
     test_image_folder = "~/Documents/GitHub/HEPiC/test/filament_images_captured"
-    default_host = "192.168.114.48"
+    default_host = "192.168.0.81"
     data_frequency = 10 # Hz, defines how many data points are recorded per time
     status_frequency = 5 # Hz, defines how often the status panel refreshes
-    tmp_data_maxlen = 1000 # length of temporarily cached data
+    tmp_data_maxlen = 100 # length of temporarily cached data
     final_data_maxlen = 10000000
