651577c0c8064f1420d716102d7eaede43e09fd2
zloverty <liuzy19911112@gmail.com>
2025-11-11 16:32:41 +0800

    separate communications into files to testable structure

--- src/communications/video_worker.py
+++ src/communications/video_worker.py
@@ -1,8 +1,18 @@
 from PySide6.QtCore import QObject, Signal, Slot, QTimer
 import numpy as np
 from pathlib import Path
+from config import Config
+import os
+from .vision import binarize, filament_diameter, convert_to_grayscale, draw_filament_contour, find_longest_branch, ImageStreamer
+import time
 
-
+if not Config.test_mode:
+    if os.name == "nt":
+        # if on windows OS, import the windows camera library
+        from hikcam_win import HikVideoCapture
+    else:
+        # on Mac / Linux, use a different library
+        from video_capture import HikVideoCapture  
 
 class VideoWorker(QObject):
     """
@@ -84,3 +94,40 @@ class VideoWorker(QObject):
             print("Test mode: exposure time setting will not have any effect.")
         else:
             self.cap = HikVideoCapture(width=512, height=512, exposure_time=exp_time*1000, center_roi=True)
+
+class ProcessingWorker(QObject):
+    """基于 distance transform 计算前景图案的尺寸。"""
+
+    proc_frame_signal = Signal(np.ndarray)
+
+    def __init__(self):
+        super().__init__()
+        self.die_diameter = np.nan
+        self.invert = False
+        
+    @Slot(np.ndarray)
+    def process_frame(self, img):
+        """Find filament in image and update the `self.die_diameter` variable with detected filament diameter."""
+        gray = convert_to_grayscale(img) # only process gray images    
+        try:
+            binary = binarize(gray)
+            if self.invert:
+                binary = cv2.bitwise_not(binary)
+            diameter, skeleton, dist_transform = filament_diameter(binary)
+            skel_px = dist_transform[skeleton]
+            skeleton_refine = skeleton.copy()
+            skeleton_refine[dist_transform < skel_px.mean()] = False
+            # filter the pixels on skeleton where dt is smaller than 0.9 of the max
+            diameter_refine = dist_transform[skeleton_refine].mean() * 2.0
+            proc_frame = draw_filament_contour(gray, skeleton_refine, diameter_refine)
+            self.proc_frame_signal.emit(proc_frame)
+            self.die_diameter = diameter_refine
+        except ValueError as e:
+            # 已知纯色图片会导致检测失败，在此情况下可以不必报错继续运行，将出口直径记为 np.nan 即可
+            print(f"图像无法处理: {e}")
+            self.proc_frame_signal.emit(binary)
+    
+    @Slot(bool)
+    def invert_toggle(self, checked):
+        """Sometimes the filament is the darker part of the image and background is brighter. In such cases, we may invert the binary image to make the algorithm work correctly. This is a toggle for the user to manually switch on/off whether to invert."""
+        self.invert = checked
